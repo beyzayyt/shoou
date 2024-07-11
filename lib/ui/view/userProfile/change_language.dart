926925fdece2f9d4a9e157ffa9fa7723d488f3df
beyzanur.yesilyurt <beyzanur.yesilyurt@defacto.com>
2024-07-11 10:05:21 +0300

    Fix

--- lib/ui/view/userProfile/change_language.dart
+++ lib/ui/view/userProfile/change_language.dart
@@ -5,50 +5,50 @@ import 'package:show_you/data/localization/local_keys.dart';
 import 'package:show_you/data/models/saved_user_model.dart';
 import 'package:show_you/ui/page/home_page.dart';
 
-// ignore: must_be_immutable
 class ChangeLanguage extends StatelessWidget {
-  ChangeLanguage({super.key, required this.savedUserModel});
-  SavedUserModel? savedUserModel;
+  final SavedUserModel? savedUserModel;
+
+  const ChangeLanguage({super.key, required this.savedUserModel});
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: () {
         showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                actions: [
-                  Center(
-                    child: Column(
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(),
-                          onPressed: () {
-                            context.setLocale(AppConstant.enlocale).then((value) => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => const HomePage()),
-                                ));
-                          },
-                          child: Text(LocaleKeys.english.tr()),
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(),
-                          onPressed: () {
-                            context.setLocale(AppConstant.trlocale).then((value) => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => const HomePage()),
-                                ));
-                          },
-                          child: Text(LocaleKeys.turkish.tr()),
-                        ),
-                      ],
-                    ),
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(),
+                    onPressed: () {
+                      context.setLocale(AppConstant.enlocale).then((value) => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const HomePage()),
+                          ));
+                    },
+                    child: Text(LocaleKeys.english.tr()),
+                  ),
+                  const SizedBox(height: 16), 
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(),
+                    onPressed: () {
+                      context.setLocale(AppConstant.trlocale).then((value) => Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const HomePage()),
+                          ));
+                    },
+                    child: Text(LocaleKeys.turkish.tr()),
                   ),
                 ],
-              );
-            });
+              ),
+            );
+          },
+        );
       },
       color: Theme.of(context).primaryColor,
       icon: Row(
